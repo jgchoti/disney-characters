bf6dea9d6a4213968651a4f2a373702dd3bef6fe
jgchoti <jgchotirat@gmail.com>
2024-05-31 17:53:06 +0200

    Create constant.js

---
+++ src/data/constant.js
@@ -0,0 +1,29 @@
+export const ICON = {
+    'Films': '<i class="fa-solid fa-film"></i>',
+    'TV Shows': '<i class="fa-solid fa-tv"></i>',
+    'Video Games': '<i class="fa-solid fa-gamepad"></i>',
+    'Short Films': '<i class="fa-solid fa-video"></i>',
+    'Park Attractions': '<i class="fa-solid fa-archway"></i>',
+    'Allies': '<i class="fa-solid fa-heart"></i>',
+    'Enemies': '<i class="fa-solid fa-heart-crack"></i>'
+}
+
+export const BTN_LIST = {
+    'btn-first': '<i class="fa-solid fa-backward-fast"></i>',
+    'btn-prev': '<i class="fa-solid fa-chevron-left"></i>',
+    'btn-next': '<i class="fa-solid fa-chevron-right"></i>',
+    'btn-last': '<i class="fa-solid fa-forward-fast"></i>'
+};
+
+export const BTN_FILTER = {
+    'all': 'Show all',
+    'films': 'Films',
+    'tvShows': 'TV Shows',
+    'videoGames': "Video Games",
+    'shortFilms': "Short Films",
+    'parkAttractions': 'Park Attractions'
+}
+
+export const ERROR_PIC = 'https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExY3JqeTZncG91eWI0bHN5MHpyeHB3NTA0Ync1aWk5cmttd29vMmR5NSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/JnH9r2A97zrqg/giphy.gif'
+export const NOT_FOUND_PIC = 'https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExbnUzZG0wN291ZzFldWpiaWNwdHo5YnFsY3d1d3NlMTVvamp6dGtuNyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/dZXFMaFBlReiA/giphy.gif'
+export const FILTER_ERROR = 'https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExcHA3NXRxbHI0a3E5MTRpYWRiMTVubXhtYmJyYWxzbmF5cXk0a3lnayZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/YOeXODDybNZg4/giphy.gif'
